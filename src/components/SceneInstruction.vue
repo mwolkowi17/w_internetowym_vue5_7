b5cafe3472f741f6ae481af2c1be4b4a04319832
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2025-11-10 12:07:47 +0100

    poprawiona unstrukcja

--- src/components/SceneInstruction.vue
+++ src/components/SceneInstruction.vue
@@ -1,69 +1,89 @@
 <script setup>
-import {  onMounted } from 'vue'
-import { instrukcjaTekst } from '../lib/instrukcja';
-defineEmits(['koniec-instrukcja', 'koniec-instrukcja-focus'])
+import { onMounted } from "vue";
+import { instrukcjaTekst } from "../lib/instrukcja";
+defineEmits(["koniec-instrukcja", "koniec-instrukcja-focus"]);
 
 const props = defineProps({
-  ifButtonOnFocus: Boolean
+  ifButtonOnFocus: Boolean,
 });
 
 onMounted(() => {
-  const elementToFocus = document.querySelector(".info-instrukcja")
+  const elementToFocus = document.querySelector(".info-instrukcja");
   //dodanie warunku propsu
   if (elementToFocus && props.ifButtonOnFocus === true) {
     elementToFocus.focus();
   }
+});
 
-})
+const textToDisplay =
+  "Gra składa się z dwóch poziomów. Na początku otrzymujesz trzy szanse." +
+  " Rzuć kostką i idź do przodu o tyle pól, ile oczek wypadło na kostce.";
 
-const textToDisplay = "Znajdujesz się w Wirtualnej Dżungli - aby ją przejść, musisz odpowiedzieć " +
-  " prawidłowo na pytania dotyczące internetu. Gra składa się z dwóch" +
-  " poziomów. Rzucasz kostką i posuwasz się do przodu o tyle pól, ile oczek" +
-  " wyrzuciła kostka. Na początku gry otrzymujesz trzy szanse &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+const pytajnikImage = new URL("../assets/ikona2question.png", import.meta.url)
+  .href;
+const textToDisplay2 =
+  "<img class='pytajnik' alt='' src=" +
+  pytajnikImage +
+  ">– na polach ze znakiem zapytania czeka cię pytanie.";
 
-const textToDisplay2 = ".  Na polach oznaczonych znakiem zapytania &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+const wykrzyknikImage = new URL(
+  "../assets/ikona3wykrzyknik.png",
+  import.meta.url
+);
 
-const textToDisplay3 = "czeka Cię pytanie." + " Każda błędna odpowiedź - to strata 1 szansy. Utrata wszystkich szans" +
-  " oznacza zakończenie gry. Dobra odpowiedź - to kolejny rzut kostką." +
-  " Po drodze czekają Cię zasadzki, kryjące się na polach oznaczonych" +
-  " wykrzyknikiem &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+const textToDisplay3 =
+  "<img class='wykrzyknik' alt='' src=" +
+  wykrzyknikImage +
+  ">– na polach z wykrzyknikiem czeka cię niespodzianka.";
 
-const textToDisplay4 = ". Powodzenia!"
+const textToDisplay4 =
+  "Poprawna odpowiedź – rzucasz kostką ponownie. Niepoprawna odpowiedź" +
+  "– tracisz jedną szansę. Jeśli wygrasz poziom 1, to otrzymujesz dodatkową szansę." +
+  "Utrata wszystkich szans oznacza koniec gry. Powodzenia!";
 
-const textToDisplayCzytnik= "Znajdujesz się w Wirtualnej Dżungli - aby ją przejść, musisz odpowiedzieć " +
-  " prawidłowo na pytania dotyczące internetu. Gra składa się z dwóch" +
-  " poziomów. Rzucasz kostką i posuwasz się do przodu o tyle pól, ile oczek" +
-  " wyrzuciła kostka. Na początku gry otrzymujesz trzy szanse"+
-  "Na polach oznaczonych znakiem zapytania"+
-  "czeka Cię pytanie."+ " Każda błędna odpowiedź - to strata 1 szansy. Utrata wszystkich szans" +
-  " oznacza zakończenie gry. Dobra odpowiedź - to kolejny rzut kostką." +
-  " Po drodze czekają Cię zasadzki, kryjące się na polach oznaczonych" +
-  " wykrzyknikiem."+
-  " Po grze możesz poruszać się za pomocą klawiszy TAB, kombinacji klawiszy Shift i TAB oraz klawisza Enter, którym zatwierdzasz swój wybór"+
-  " W grze usłyszysz także sygnały dźwiękowe, oznaczające odpowiednio ruch pionka, prawidłową lub złą odpwiedź, wygraną etapu lub wygraną całej gry oraz dźwięk"+
-   "informujący cię o przegranej"
-  " Powodzenia"
-  //dodać informacje dla osób niewidzących
+const textToDisplayCzytnik =
+  "Zasady gry " +
+  "Gra składa się z dwóch poziomów. Na początku otrzymujesz trzy szanse." +
+  " Rzuć kostką i idź do przodu o tyle pól, ile oczek wypadło na kostce." +
+  "– na polach ze znakiem zapytania czeka cię pytanie." +
+  "– na polach z wykrzyknikiem czeka cię niespodzianka." +
+  "Poprawna odpowiedź – rzucasz kostką ponownie. Niepoprawna odpowiedź" +
+  "– tracisz jedną szansę. Jeśli wygrasz poziom 1, to otrzymujesz dodatkową szansę." +
+  "Utrata wszystkich szans oznacza koniec gry. Powodzenia!" +
+  " Po grze możesz poruszać się za pomocą klawiszy TAB, kombinacji klawiszy Shift i TAB oraz klawisza Enter, którym zatwierdzasz swój wybór" +
+  " W grze usłyszysz także sygnały dźwiękowe, oznaczające odpowiednio ruch pionka, prawidłową lub złą odpwiedź, wygraną etapu lub wygraną całej gry oraz dźwięk" +
+  "informujący cię o przegranej";
 
+//dodać informacje dla osób niewidzących
 </script>
 <template>
   <div class="tlo1" aria-label="Zasady gry">
-    <div class="info-instrukcja" tabindex="0" :aria-label="textToDisplayCzytnik">
-      <h1 class="instrukcja-title" >Zasady gry</h1>
-      <div class="kontener-instrukcja" >
-        <span class="instrukcja" v-html="textToDisplay"></span>
-        <img class="gwiazdka" alt="" src="../assets/ikona1gwiazdka.png" />
-        <span class="instrukcja" v-html="textToDisplay2"></span>
-        <img class="pytajnik" alt="" src="../assets/ikona2question.png" />
-        <span class="instrukcja" v-html="textToDisplay3"></span>
-        <img class="wykrzyknik" alt="" src="../assets/ikona3wykrzyknik.png" />
-        <span class="instrukcja" v-html="textToDisplay4"></span>
+    <div
+      class="info-instrukcja"
+      tabindex="0"
+      :aria-label="textToDisplayCzytnik"
+    >
+      <h1 class="instrukcja-title">Zasady gry</h1>
+      <div class="kontener-instrukcja">
+        <p class="instrukcja" v-html="textToDisplay"></p>
+        <!-- <img class="gwiazdka" alt="" src="../assets/ikona1gwiazdka.png" />-->
+        <!-- <img class="pytajnik" alt="" src="../assets/ikona2question.png" /> -->
+        <p class="instrukcja" v-html="textToDisplay2"></p>
+
+        <p class="instrukcja" v-html="textToDisplay3"></p>
+        <!-- <img class="wykrzyknik" alt="" src="../assets/ikona3wykrzyknik.png" /> -->
+        <p class="instrukcja" v-html="textToDisplay4"></p>
       </div>
     </div>
   </div>
-  <button class="dalej anim1" @click="$emit('koniec-instrukcja')" @keydown.enter="$emit('koniec-instrukcja-focus')"
-    role="button">Dalej</button>
-
+  <button
+    class="dalej anim1"
+    @click="$emit('koniec-instrukcja')"
+    @keydown.enter="$emit('koniec-instrukcja-focus')"
+    role="button"
+  >
+    Dalej
+  </button>
 </template>
 
 <style scoped>
@@ -83,7 +103,6 @@ const textToDisplayCzytnik= "Znajdujesz się w Wirtualnej Dżungli - aby ją prz
   top: 120px;
   left: 197px;
   position: absolute;
-
 }
 
 .info-instrukcja:focus {
@@ -103,12 +122,11 @@ const textToDisplayCzytnik= "Znajdujesz się w Wirtualnej Dżungli - aby ją prz
   text-align: center;
 }
 
-
 .kontener-instrukcja {
   /* display: flex;
   align-items: center; */
   display: inline-block;
-  padding: .5rem 1rem;
+  padding: 0.5rem 1rem;
   /* width: 1300px; */
   width: 1400px;
   top: 90px;
@@ -124,9 +142,8 @@ const textToDisplayCzytnik= "Znajdujesz się w Wirtualnej Dżungli - aby ją prz
   font-family: "Proxima Nova", sans-serif;
   line-height: 1.7;
   position: relative;
-
+  margin-bottom: -20px;
 }
-
 
 .gwiazdka {
   position: relative;
@@ -135,29 +152,46 @@ const textToDisplayCzytnik= "Znajdujesz się w Wirtualnej Dżungli - aby ją prz
   margin-top: -45px;
   margin-right: -95px;
   left: -98px;
-  top: 35px
+  top: 35px;
 }
 
 .pytajnik {
-  position: relative;
-  /* top: 283px;
-  left: 355px; */
-  margin-top: -38px;
+  /* position: absolute; */
+  top: 283px;
+  left: 1355px;
+  /* margin-top: -38px;
   margin-right: -80px;
-  left: -90px;
-  top: 35px
+  left: -90px; */
+  /*top: 35px;*/
+}
+
+.kontener-instrukcja :deep(.pytajnik) {
+  position: relative;
+
+  margin-top: -308px;
+  margin-right: -10px;
+  left: -10px;
+  top: 35px;
 }
 
 .wykrzyknik {
   position: relative;
   /* top: 475px;
   left: 715px; */
-  margin-top: -38px;
+  /* margin-top: -38px;
   margin-right: -80px;
-  left: -90px;
-  top: 35px
+  left: -90px; */
+  /* top: 35px;*/
 }
 
+.kontener-instrukcja :deep(.wykrzyknik) {
+  position: relative;
+
+  margin-top: -308px;
+  margin-right: -10px;
+  left: -10px;
+  top: 35px;
+}
 .dalej {
   color: rgb(29, 56, 80);
   font-size: 70px;
@@ -176,8 +210,7 @@ const textToDisplayCzytnik= "Znajdujesz się w Wirtualnej Dżungli - aby ją prz
   box-sizing: border-box;
   overflow: hidden;           /* Prevent overflow *
   transform-origin: center;  */
-  transition: .2s ease-in-out;
-
+  transition: 0.2s ease-in-out;
 }
 
 .dalej:hover {
@@ -192,7 +225,6 @@ const textToDisplayCzytnik= "Znajdujesz się w Wirtualnej Dżungli - aby ją prz
 
 /* The animation code */
 @keyframes example {
-
   from {
     opacity: 0;
   }
